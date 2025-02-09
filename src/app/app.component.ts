d2cc98767cfca1105fbc93f4a4e8c567109a8f51
Amanda Caroline da Silva Rodrigues <amandacsr10@gmail.com>
2025-02-09 16:13:16 -0300

    add header and carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
